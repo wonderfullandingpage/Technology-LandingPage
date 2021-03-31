059a89796693e63216acfbbc6fddc7bf4ca5e3d9
lindelof <phragmen@protonmail.com>
2021-03-31 16:53:42 +0800

    Update readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment, Suspense, lazy } from "react";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import theme from "./theme";
 import GlobalStyles from "./GlobalStyles";
 import Pace from "./shared/components/Pace";
@@ -11,7 +11,7 @@ const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
         <GlobalStyles />
@@ -27,7 +27,7 @@ function App() {
           </Switch>
         </Suspense>
       </MuiThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
